c91cd01e116640d9a0a61f306832f369ff1bab1e
Ulad Kasach <uladkasach@gmail.com>
2024-05-25 20:08:42 -0400

    feat(cast): toMillisecondsSinceEpoch

---
+++ src/casts/toMillisecondsSinceEpoch.ts
@@ -0,0 +1,7 @@
+import { UniDate, UniDateTime } from '../domain/UniDateTime';
+
+export const toMillisecondsSinceEpoch = (
+  input: UniDate | UniDateTime,
+): number => new Date(input).getTime();
+
+export { toMillisecondsSinceEpoch as toMse };
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './domain/UniDuration';
 export * from './utils/sleep';
 export * from './checks/isUniDate';
 export * from './checks/isUniDateTime';
+export * from './casts/toMillisecondsSinceEpoch';
