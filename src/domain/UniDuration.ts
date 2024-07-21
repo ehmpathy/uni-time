49805d412a3f0c79a7d88d2bb3c8e162124637b4
Ulad Kasach <uladkasach@gmail.com>
2024-07-20 20:40:51 -0400

    fix(duration): handle zero unit durations correctly

--- src/domain/UniDuration.ts
+++ src/domain/UniDuration.ts
@@ -1,6 +1,6 @@
 import { UnexpectedCodePathError } from '@ehmpathy/error-fns';
 import { AsOfGlossary } from 'domain-glossaries';
-import { PickOne } from 'type-fns';
+import { PickOne, isPresent } from 'type-fns';
 
 export type UniDuration = AsOfGlossary<
   PickOne<{
@@ -15,11 +15,11 @@ export type UniDuration = AsOfGlossary<
 >;
 
 export const toMilliseconds = (duration: UniDuration): number => {
-  if (duration.days) return duration.days * 24 * 60 * 60 * 1000;
-  if (duration.hours) return duration.hours * 60 * 60 * 1000;
-  if (duration.minutes) return duration.minutes * 60 * 1000;
-  if (duration.seconds) return duration.seconds * 1000;
-  if (duration.milliseconds) return duration.milliseconds;
+  if (isPresent(duration.days)) return duration.days * 24 * 60 * 60 * 1000;
+  if (isPresent(duration.hours)) return duration.hours * 60 * 60 * 1000;
+  if (isPresent(duration.minutes)) return duration.minutes * 60 * 1000;
+  if (isPresent(duration.seconds)) return duration.seconds * 1000;
+  if (isPresent(duration.milliseconds)) return duration.milliseconds;
   throw new UnexpectedCodePathError(
     'unsupported duration unit to convert to milliseconds',
     { duration },
